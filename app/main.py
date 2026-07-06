4f854cdcb0310299edbaecbc0756e3e263da8a9a
mulykoV <vovamulyka@gmail.com>
2026-07-06 17:58:48 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,18 +1,17 @@
-import os
-
-
 def copy_file(command: str) -> None:
     parts = command.split()
 
-    if len(parts) < 3 or parts[0] != "cp":
+    if len(parts) != 3 or parts[0] != "cp":
         return
 
+    source_name = parts[1]
+    destination_name = parts[2]
     if parts[1] == parts[2]:
         return
-    old_file = parts[1]
-    new_file = parts[2]
-    if not os.path.exists(old_file):
-        return
 
-    with open(old_file, "r") as source, open(new_file, "w") as destination:
-        destination.write(source.read())
+    try:
+        with open(source_name, "r") as source, \
+                open(destination_name, "w") as destination:
+            destination.write(source.read())
+    except FileNotFoundError:
+        return
